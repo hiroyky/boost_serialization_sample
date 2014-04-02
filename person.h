12c960e81ae0bb3990d3a8172fa55d1be27c6cfd
Hirokazu Yokoyama <hirokazu.online@gmail.com>
2014-04-02 19:29:30 +0900

    list出力の修正

--- person.h
+++ person.h
@@ -37,7 +37,7 @@ private:
   friend class boost::serialization::access;
   template <class Archive>
     void serialize(Archive& ar, const unsigned int version) {
-    ar & boost::serialization::make_nvp("person", *this);
+    ar & boost::serialization::make_nvp("person", list);
   }
 };
 
